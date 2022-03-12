c6b63af13822387cca1e20179c0fe4f9f674f655
hugosenari <hugosenari@gmail.com>
2022-03-12 01:31:50 -0300

    chore: re index kashae

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Calculation caching library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."kashae-master".url = "path:./master";
+  inputs."kashae-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."kashae-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
